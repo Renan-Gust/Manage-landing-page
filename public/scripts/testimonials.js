2340091c7ee73dcc5957b96959ad899d28fc04b5
Renan Gustavo <renandevfullstack@gmail.com>
2021-05-26 15:17:40 -0300

    updating the testimonials

---
+++ public/scripts/testimonials.js
@@ -0,0 +1,55 @@
+const c = element => document.querySelector(element)
+const cs = element => document.querySelectorAll(element)
+
+testimonialsJsonDesktop.map((item) => {
+    let testimonialsItem = c(".testimonials-content .testimonials-person").cloneNode(true)
+    c(".testimonials-desktop-area").append(testimonialsItem)
+
+    testimonialsItem.querySelector(".testimonials-desktop-area .testimonials-person img").src = item.img
+    testimonialsItem.querySelector(".testimonials-desktop-area .testimonials-person h2").innerHTML = item.name
+    testimonialsItem.querySelector(".testimonials-desktop-area .testimonials-person p").innerHTML = item.description
+})
+
+testimonialsJsonMobile.map((item) => {
+    let testimonialsItem = c(".testimonials-content .testimonials-person").cloneNode(true)
+    c(".testimonials-mobile-area .testimonials-mobile").append(testimonialsItem)
+
+    testimonialsItem.querySelector(".testimonials-mobile-area .testimonials-mobile img").src = item.img
+    testimonialsItem.querySelector(".testimonials-mobile-area .testimonials-mobile h2").innerHTML = item.name
+    testimonialsItem.querySelector(".testimonials-mobile-area .testimonials-mobile p").innerHTML = item.description
+
+    let spanElement = document.createElement("span")
+    c(".testimonials-mobile-area .index").append(spanElement)
+})
+
+c(".testimonials-mobile .testimonials-person").classList.add("selected")
+c(".testimonials-mobile-area .index span").classList.add("active")
+
+function executeSlider() {
+    let people = cs(".testimonials-mobile .testimonials-person")
+    let indexActive = cs(".testimonials-mobile-area .index span")
+    let max = people.length
+    let indexCurrent = 0
+
+    function nextPerson() {
+        people[indexCurrent].classList.remove("selected")
+        indexActive[indexCurrent].classList.remove("active")
+
+        indexCurrent++
+        if(indexCurrent >= max) indexCurrent = 0
+
+        people[indexCurrent].classList.add("selected")
+        indexActive[indexCurrent].classList.add("active")
+    }
+
+    function start() {
+        setInterval(() => {
+            nextPerson()
+        }, 2000);
+    }
+
+    window.addEventListener("load", start)
+}
+    
+const screenWidth = window.screen.width
+if(screenWidth >= 320 && screenWidth <= 1024) executeSlider
---
+++ public/scripts/testimonialsJson.js
@@ -0,0 +1,40 @@
+let testimonialsJsonDesktop = [
+    {
+        name: "Anisha Li",
+        img: "./public/images/avatar-anisha.png",
+        description: "Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."
+    },
+    {
+        name: "Ali Bravo",
+        img: "./public/images/avatar-ali.png",
+        description: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."
+    },
+    {
+        name: "Richard Watts",
+        img: "./public/images/avatar-richard.png",
+        description: "Manage allows us to provide structure and process It keeps us organized and focused. I can't stop recommending them to everyone I talk to!"
+    }
+]
+
+let testimonialsJsonMobile = [
+    {
+        name: "Anisha Li",
+        img: "./public/images/avatar-anisha.png",
+        description: "Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."
+    },
+    {
+        name: "Ali Bravo",
+        img: "./public/images/avatar-ali.png",
+        description: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."
+    },
+    {
+        name: "Richard Watts",
+        img: "./public/images/avatar-richard.png",
+        description: "Manage allows us to provide structure and process It keeps us organized and focused. I can't stop recommending them to everyone I talk to!"
+    },
+    {
+        name:"Shanai Gough",
+        img:"./public/images/avatar-shanai.png",
+        description: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."
+    }
+]
